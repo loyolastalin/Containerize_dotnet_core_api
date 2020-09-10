d9794b026fd4422381b0ee8998e0d314a3089f83
loyolastalin <loyolastalin@gmail.com>
2020-09-10 16:38:17 +0530

    - updated

--- docker-run.sh
+++ docker-run.sh
@@ -6,5 +6,5 @@ PKG_VERSION=1.0
 
 docker build -f Dockerfile --pull=true -t ${IMG_TAG}  .
 
-docker run --rm --name $CONTINER_NAME $IMG_TAG -e PKG_VERSION=${PKG_VERSION}
+docker run -p 4040:80 --rm --name $CONTINER_NAME $IMG_TAG -e PKG_VERSION=${PKG_VERSION}
 
